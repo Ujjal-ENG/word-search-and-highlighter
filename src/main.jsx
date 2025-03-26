73887ffded5a35cac76663f29f27c9ce5a99388f
ujjal_roy <ujjalroy7862@gmail.com>
2025-03-26 19:53:39 +0600

    adding my react-tempalte

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Import global styles
+
+// Import store for Redux
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+                        <App />
+              
+);
